1ada693cd0c4ed1441a9f09239989f4e370a5405
rpeignet <you@example.com>
2024-02-06 17:12:56 +0100

    add page link

--- app/src/main/java/fr/esgi/trainstation/MainActivity.kt
+++ app/src/main/java/fr/esgi/trainstation/MainActivity.kt
@@ -1,11 +1,27 @@
 package fr.esgi.trainstation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.esgi.trainstation.databinding.ActivityMainBinding
+import fr.esgi.trainstation.rptTestActivity.ActivityRptTest
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btnLottie.setOnClickListener{
+            val intent = Intent(this, ActivityRptTest::class.java)
+            print(intent.action);
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            startActivity(intent)
+        }
+
+        setContentView(binding.root)
     }
+
 }
---
+++ app/src/main/java/fr/esgi/trainstation/rptTestActivity/ActivityRptTest.kt
@@ -0,0 +1,12 @@
+package fr.esgi.trainstation.rptTestActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.esgi.trainstation.R
+
+class ActivityRptTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rpt_test)
+    }
+}
